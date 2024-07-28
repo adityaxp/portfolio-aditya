675a5868fd6713bc087440f47f0d186f317cd047
aditya <adityabalsane10@gmail.com>
2024-07-28 22:58:23 +0530

    config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,9 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss'
+
+const svgToDataUri = require('mini-svg-data-uri')
+const {
+  default: flattenColorPalette
+} = require('tailwindcss/lib/util/flattenColorPalette')
 
 const config = {
   darkMode: ['class'],
@@ -6,29 +11,29 @@ const config = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
   prefix: '',
   theme: {
     container: {
       center: true,
-      padding: '15px',
+      padding: '15px'
     },
     screens: {
       sm: '640px',
       md: '768px',
       lg: '960px',
-      xl: '1200px',
+      xl: '1200px'
     },
     fontFamily: {
-      primary: 'var(--font-jetbrainsMono)',
+      primary: 'var(--font-jetbrainsMono)'
     },
     extend: {
       colors: {
         primary: '#1c1c22',
         accent: {
           DEFAULT: '#08ff99',
-          hover: '#00e187',
+          hover: '#00e187'
         },
 
         border: 'hsl(var(--border))',
@@ -39,48 +44,74 @@ const config = {
 
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          foreground: 'hsl(var(--secondary-foreground))'
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+          foreground: 'hsl(var(--destructive-foreground))'
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
+          foreground: 'hsl(var(--muted-foreground))'
         },
 
         popover: {
           DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
+          foreground: 'hsl(var(--popover-foreground))'
         },
         card: {
           DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
+          foreground: 'hsl(var(--card-foreground))'
+        }
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
         'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+          from: {height: '0'},
+          to: {height: 'var(--radix-accordion-content-height)'}
         },
         'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
+          from: {height: 'var(--radix-accordion-content-height)'},
+          to: {height: '0'}
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
-    },
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
+    }
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [
+    addVariablesForColors,
+    function ({matchUtilities, theme}: any) {
+      matchUtilities(
+        {
+          'bg-dot-thick': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`
+          })
+        },
+        {values: flattenColorPalette(theme('backgroundColor')), type: 'color'}
+      )
+    },
+    require('tailwindcss-animate')
+  ]
 } satisfies Config
+
+function addVariablesForColors({addBase, theme}: any) {
+  let allColors = flattenColorPalette(theme('colors'))
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  )
+
+  addBase({
+    ':root': newVars
+  })
+}
 
 export default config
